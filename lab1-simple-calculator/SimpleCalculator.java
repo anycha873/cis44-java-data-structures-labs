ca47dd138fd52e81d9365d4f484fe74f906fca55
anycha873 <143572362+anycha873@users.noreply.github.com>
2026-02-20 14:52:38 -0800

    Update SimpleCalculator.java

--- lab1-simple-calculator/SimpleCalculator.java
+++ lab1-simple-calculator/SimpleCalculator.java
@@ -1,1 +1,81 @@
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double screen = 0.0;
+        double stored = 0.0;
+        String op = "";
+
+        System.out.println("Calculator started. Type exit to quit.");
+        System.out.println("Screen: " + screen);
+
+        while (true) {
+
+            String input = sc.nextLine();
+
+
+            if (input.equals("exit")) {
+                break;
+            }
+
+
+            else if (input.equals("+")) {
+                stored = screen;
+                op = "+";
+            }
+
+
+            else if (input.equals("-")) {
+                stored = screen;
+                op = "-";
+            }
+
+
+            else if (input.equals("*")) {
+                stored = screen;
+                op = "*";
+            }
+
+
+            else if (input.equals("/")) {
+                stored = screen;
+                op = "/";
+            }
+
+
+            else if (input.equals("=")) {
+
+                if (op.equals("+")) {
+                    screen = stored + screen;
+                }
+
+                else if (op.equals("-")) {
+                    screen = stored - screen;
+                }
+
+                else if (op.equals("*")) {
+                    screen = stored * screen;
+                }
+
+                else if (op.equals("/")) {
+                    screen = stored / screen;
+                }
+            }
+
+
+            else {
+                screen = Double.parseDouble(input);
+            }
+
+
+            System.out.println("Screen: " + screen);
+        }
+
+
+        System.out.println("Calculator closed.");
+    }
+}
